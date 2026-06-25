da44910527a6169b89b379df19c862294f529ad3
Himanshu Bhandary <2032ushimanshu@gmail.com>
2026-06-25 20:04:48 +0530

    Remove tracked wasm build artifacts

--- physics_wasm/target/wasm32-unknown-unknown/release/deps/physics_wasm.d
+++
@@ -1,5 +0,0 @@
-/home/himanshu/BiGI/physics_wasm/target/wasm32-unknown-unknown/release/deps/physics_wasm.d: src/lib.rs
-
-/home/himanshu/BiGI/physics_wasm/target/wasm32-unknown-unknown/release/deps/physics_wasm.wasm: src/lib.rs
-
-src/lib.rs:
--- physics_wasm/target/wasm32-unknown-unknown/release/physics_wasm.d
+++
@@ -1,1 +0,0 @@
-/home/himanshu/BiGI/physics_wasm/target/wasm32-unknown-unknown/release/physics_wasm.wasm: /home/himanshu/BiGI/physics_wasm/src/lib.rs
